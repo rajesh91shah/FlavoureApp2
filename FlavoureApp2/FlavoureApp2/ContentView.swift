cd628548170cbfb9a5d53d7c5fb7fd615563fb66
rajesh91shah@gmail.com <apple@Tests-MacBook-Pro.local>
2019-12-20 23:21:46 +0530

    first push

---
+++ FlavoureApp2/FlavoureApp2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlavoureApp2
+//
+//  Created by Rajesh Shah on 20/12/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
