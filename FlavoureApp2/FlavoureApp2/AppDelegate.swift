e8669f6cfe9fd4da890c03859012f6ee0a2eef8c
rajesh91shah@gmail.com <apple@Tests-MacBook-Pro.local>
2019-12-21 00:31:09 +0530

    Demo done

--- FlavoureApp2/FlavoureApp2/AppDelegate.swift
+++ FlavoureApp2/FlavoureApp2/AppDelegate.swift
@@ -15,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if let flv = Bundle.main.infoDictionary?["Environment"] as? String {
+            print("\(flv) value")
+        }
+        else {
+            print(" Value not found")
+        }
+        
+        
         return true
     }
 
